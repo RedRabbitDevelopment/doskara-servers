27e178ce12f5d5ad3dd2df593a8f6c4e8fea628c
Jay Hendren <james.g.hendren@gmail.com>
2014-05-02 15:46:40 -0600

    updated start container script

--- hosts/dynos/start-container.sh
+++ hosts/dynos/start-container.sh
@@ -10,9 +10,17 @@ then start APPNAME, linking in DEPENDENCIES
 # check for proper number of arguments
 if [[ $# -lt 1 ]]; then echo $usage ; fi
 
+# store name of this script
 start_container="$0"
+
+# store name of the app to start and remove it from the arguments list
 app="$1"
 shift
+
+# TODO:
+# fetch list of dependencies from mongo instead instead of looking for them on the command line
+# e.g.:
+# depslist="$(mongo -c <query to fetch dependencies>)"
 
 # start all dependencies
 link_args=()
