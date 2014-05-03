5e87229af84fc534c561d978c88d3889319bb780
Nathan Tate <yourdeveloperfriend@gmail.com>
2014-05-03 21:57:58 +0000

    Added things to factory

--- hosts/factory/build.sh
+++ hosts/factory/build.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-
+set -e
+echo "Building project..."
 # sanity checks
 app_name="$1"
 appdir="/tmp/doskara-apps/${app_name}"
 
 # check to see if directory already exists
-
+echo "Making directory"
 dockerfile="${appdir}/Dockerfile"
 mkdir -p "${appdir}"
 
 # read input into tar file
+echo "Reading tarball"
 tarname=build.tar.gz
 tarfile="${appdir}/${tarname}"
 cat > "${tarfile}"
 
 
 # create dockerfile
+echo "Making dockerfile"
 echo "
 FROM ubuntu:latest
 ENV DEBIAN_FRONTEND noninteractive
@@ -29,10 +32,13 @@ ENTRYPOINT /bin/bash ./run.sh
 " > "${dockerfile}"
 
 # build docker container
+echo "Building docker container"
 docker build -t "warehouse:5000/${app_name}" "${appdir}"
 
 # push to build host
+echo "Pushing container to repository"
 docker push "warehouse:5000/${app_name}"
 
 # clean up
+echo "Cleaning up"
 rm -rf "${appdir}"
