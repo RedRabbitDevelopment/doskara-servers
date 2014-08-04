a366df51a078f92df982bf4e5a6e302489bfb82a
Nathan Tate <yourdeveloperfriend@gmail.com>
2014-08-04 22:01:01 +0000

    Added

--- hosts/warden/start-structure-host.sh
+++ hosts/warden/start-structure-host.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 
-read id ip <<< $(aws ec2 run-instances --image-id ami-9cbdd2ac --security-group-ids sg-4b894a2e --instance-type t1.micro --subnet-id subnet-18739e7d --output text --query 'Instances[*].[InstanceId,PrivateIpAddress]')
-
-echo "New instance id is $id"
-echo "New instance ip is $ip"
