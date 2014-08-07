49f3a7d42f2685a406aa110f1c2cb10c55555585
Nathan Tate <yourdeveloperfriend@gmail.com>
2014-08-07 21:14:48 +0000

    A few factory changes

--- mongo-queue/index.js
+++ mongo-queue/index.js
@@ -133,6 +133,7 @@ module.exports = MongoQueue = {
           }
         }).then(function(result) {
           if(doc.expectResponse) {
+console.log('emitting response', result);
             return MongoQueue.emit({
               event: doc.event + '-complete',
               id: doc.id,
