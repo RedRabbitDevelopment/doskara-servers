ad91b2bd99ecfbb3342b8f93352bc3c6c09a34ff
Nathan Tate <yourdeveloperfriend@gmail.com>
2014-08-07 23:20:36 +0000

    Update running variable

--- warden/deploy.js
+++ warden/deploy.js
@@ -11,7 +11,7 @@ Queue.on('deploy', function(doc) {
     image: atomName
   }).then(function(atom) {
     writeStream.write('Got atom');
-    return Q.nfcall(exec, 'aws ec2 run-instances --image-id ami-53a8d263 --security-group-ids sg-00810465 --instance-type t2.micro --subnet-id subnet-03739e66 --output text --query "Instances[*].[InstanceId,PrivateIpAddress]"')
+    return Q.nfcall(exec, 'aws ec2 run-instances --image-id ami-b3a2d883 --security-group-ids sg-00810465 --instance-type t2.micro --subnet-id subnet-03739e66 --output text --query "Instances[*].[InstanceId,PrivateIpAddress]"')
     .then(function(output) {
       output = output[0].split('\t');
       var newInstanceId = output[0];
@@ -32,8 +32,11 @@ Queue.on('deploy', function(doc) {
         return Q.ninvoke(atoms, 'update', {
           _id: atom._id
         }, {
-          instanceId: newInstanceId,
-          ipAddress: newIp
+          $set: {
+            running: true,
+            instanceId: newInstanceId,
+            ipAddress: newIp
+          }
         });
       });
     });
