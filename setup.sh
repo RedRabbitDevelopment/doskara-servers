ed39db09fe2037c86d6449e010e5c510a510f952
Nathan Tate <nathantate88@gmail.com>
2014-08-07 17:07:15 -0600

    Updated

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
-
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
 sudo apt-get install git-core
-chmod 755 ./gitreceive
-chown root:root ./gitreceive
-sudo mv ./gitreceive /usr/bin
-sudo gitreceive init
+sudo apt-get install nodejs
+sudo apt-get install mongodb-clients
+sudo mkdir /usr/local/doskara
+cd /usr/local/doskara
+sudo chown root:ubuntu -R /usr/local/doskara
+sudo chmod g+w -R /usr/local/doskara
+git clone git@github.com:RedRabbitDevelopment/doskara-servers.git
+cd /usr/local/doskara/doskara-servers
+
+# Docker
+# http://docs.docker.com/installation/ubuntulinux/
+sudo apt-get update
+sudo apt-get install docker.io
+sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
