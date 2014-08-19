eac5a29701d189099a1bda1190c7dd5da5e905c4
Nathan Tate <nathantate88@gmail.com>
2014-08-19 14:03:35 -0600

    Correct image

--- warden/deploy.js
+++ warden/deploy.js
@@ -14,7 +14,7 @@ Queue.on('deploy', function(doc) {
     image: atomName
   }).then(function(atom) {
     writeStream.write('Deploying aws ec2 instance');
-    return Q.nfcall(exec, 'aws ec2 run-instances --image-id ami-0789cc37 --security-group-ids sg-00810465 --instance-type t2.micro --subnet-id subnet-03739e66 --output text --query "Instances[*].[InstanceId,PrivateIpAddress]"')
+    return Q.nfcall(exec, 'aws ec2 run-instances --image-id ami-77216547 --security-group-ids sg-00810465 --instance-type t2.micro --subnet-id subnet-03739e66 --output text --query "Instances[*].[InstanceId,PrivateIpAddress]"')
     .then(function(output) {
       output = output[0].split('\t');
       var newInstanceId = output[0];
