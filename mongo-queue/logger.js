09fcb836809c06ad172e8fbcc3c88b5e43dbf5ca
Nathan Tate <nathantate88@gmail.com>
2014-08-19 15:15:26 -0600

    Logged

--- mongo-queue/logger.js
+++ mongo-queue/logger.js
@@ -5,12 +5,10 @@ var mongo = require('./mongo');
 var Logger = module.exports = function(name) {
   this.processes = [];
   this.idPromise = this.getCollection().then(function(logs) {
-    console.log('got logs');
     return Q.ninvoke(logs, 'insert', {
       name: name
     });
   }).then(function(log) {
-    console.log('inserted', log[0]._id);
     return log[0]._id;
   });
 };
@@ -24,7 +22,6 @@ Logger.prototype = {
       this.getCollection(),
       this.idPromise
     ]).spread(function(logs, id) {
-      console.log('here', id);
       return Q.ninvoke(logs, 'update', {_id: id}, {
         $push: {messages: args}
       });
