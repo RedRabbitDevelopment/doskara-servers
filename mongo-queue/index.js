ac1e935f246c920b4144300a2cd0b274b21bcd03
Nathan Tate <nathantate88@gmail.com>
2014-08-23 16:16:33 -0600

    Removed stupid comma

--- mongo-queue/index.js
+++ mongo-queue/index.js
@@ -61,7 +61,7 @@ module.exports = MongoQueue = {
     }
     var listener = MongoQueue.on({
       event: 'write-stream',
-      streamId: streamId,
+      streamId: streamId
     }, {
       timePeriod: 100,
       maxProcessing: 20,
